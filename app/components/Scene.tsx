4806e253c3eab9b0c76822776a39f1b088e979db
Alan Paccor <alanpaccor@gmail.com>
2025-02-11 16:40:01 -0500

    vercel deploy fix again 5

--- app/components/Scene.tsx
+++ app/components/Scene.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls, Stage, useGLTF, PrimitiveProps } from '@react-three/drei'
+import { OrbitControls, Stage, useGLTF } from '@react-three/drei'
 import { Suspense } from 'react'
 import { Object3D } from 'three'
 
